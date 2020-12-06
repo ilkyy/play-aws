a4b7d74e905b44194340d6f488002fc2eba8ae3d
Ilkay Yilmaz <ilkayyilmaz34@gmail.com>
2020-12-06 23:15:01 +0300

    first commit

---
+++ src/main/java/com/playaws/playaws/PlayawsApplication.java
@@ -0,0 +1,13 @@
+package com.playaws.playaws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlayawsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlayawsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/playaws/playaws/PlayawsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.playaws.playaws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlayawsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

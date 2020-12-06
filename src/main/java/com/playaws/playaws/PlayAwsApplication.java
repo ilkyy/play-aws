35129057d9f05e4b000877c92d2da6d3ba5bdfb8
Ilkay Yilmaz <ilkayyilmaz34@gmail.com>
2020-12-06 23:34:40 +0300

    first commit

--- src/main/java/com/playaws/playaws/PlayAwsApplication.java
+++ src/main/java/com/playaws/playaws/PlayAwsApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class PlayawsApplication {
+public class PlayAwsApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(PlayawsApplication.class, args);
+        SpringApplication.run(PlayAwsApplication.class, args);
     }
 
 }
